474f2da8d4f6f99d87fb3bceb3ddc16980a2a90f
Erock - Generock Studios <120332380+GenerockStudios@users.noreply.github.com>
2024-10-19 22:58:48 +0300

    Setup Auth component

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Erflix
  (Eric + Netflix = Erflix) This project is personalized netflix, with ASP.NET CORE C# & NEXT JS
+ 
+ ## French documentation
+
+ ### Description 
+ Ce projet est un clone de Netflix
---
+++ erflix-frontend/app/auth/page.tsx
@@ -0,0 +1,12 @@
+
+/**
+ * Composant qui permet l'authentification utilisateur
+ * @returns 
+ */
+export default function AuthPage() {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')]">
+
+        </div>
+    )
+}
